2c5a4ef01d59ca70e79311bbff7d7ceb7129719f
nomelancholy <starmekey@naver.com>
2020-08-18 15:31:34 +0900

    test with spring annotation

--- java-hello-test/src/main/java/org/example/CalculatorService.java
+++ java-hello-test/src/main/java/org/example/CalculatorService.java
@@ -1,7 +1,9 @@
 package org.example;
 
-public class CalculatorService {
+import org.springframework.stereotype.Component;
 
+@Component
+public class CalculatorService {
 
     public int plus(int value1, int value2) {
         return value1 + value2;
