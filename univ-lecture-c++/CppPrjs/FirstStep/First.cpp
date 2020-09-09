8aad436e80bf7dbdae2ad67273e0320d2526af74
nomelancholy <starmekey@naver.com>
2020-09-10 00:02:20 +0900

    first c++ project

---
+++ univ-lecture-c++/CppPrjs/FirstStep/First.cpp
@@ -0,0 +1,11 @@
+//
+// Created by starm on 2020-09-09.
+//
+
+#include "iostream"
+
+int main()
+{
+    std::cout << "나의 첫번째 c++ 프로그램" << std::endl;
+    return 0;
+}
---
+++ univ-lecture-c++/CppPrjs/cmake-build-debug/CMakeFiles/CppPrjs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppPrjs.dir/FirstStep/First.obj"
+  "CppPrjs.exe"
+  "CppPrjs.exe.manifest"
+  "CppPrjs.pdb"
+  "libCppPrjs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppPrjs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
