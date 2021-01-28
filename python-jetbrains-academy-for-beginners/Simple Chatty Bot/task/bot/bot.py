04f83f816eae26ea6fc79beab605d5848dc07013
nomelancholy <starmekey@naver.com>
2021-01-29 08:25:25 +0900

    completed project 2

--- python-jetbrains-academy-for-beginners/Simple Chatty Bot/task/bot/bot.py
+++ python-jetbrains-academy-for-beginners/Simple Chatty Bot/task/bot/bot.py
@@ -2,6 +2,6 @@ print('Hello! My name is Aid.')
 print('I was created in 2020.')
 print('Please, remind me your name.')
 
-# reading a name
+your_name = input()  # reading a name
 
-print('What a great name you have, {your_name}!')
+print('What a great name you have, ' + your_name + '!')
