03a517b91e445eb1cdc9ea7db60b41ac75d07863
nomelancholy <starmekey@naver.com>
2021-01-31 20:26:35 +0900

    completed integer arithmetic topic

--- python-jetbrains-academy-for-beginners/Simple Chatty Bot/task/bot/bot.py
+++ python-jetbrains-academy-for-beginners/Simple Chatty Bot/task/bot/bot.py
@@ -2,6 +2,12 @@ print('Hello! My name is Aid.')
 print('I was created in 2020.')
 print('Please, remind me your name.')
 
-your_name = input()  # reading a name
+name = input()
 
-print('What a great name you have, ' + your_name + '!')
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+
+print("Your age is {your_age}; that's a good time to start programming!")
