ac650ece06bd185d2c2666f1a8aa9577b08f4fb2
nomelancholy <starmekey@naver.com>
2021-02-01 18:14:33 +0900

    completed topic until comparisons

--- python-jetbrains-academy-for-beginners/Simple Chatty Bot/task/bot/bot.py
+++ python-jetbrains-academy-for-beginners/Simple Chatty Bot/task/bot/bot.py
@@ -8,11 +8,15 @@ print('What a great name you have, ' + name + '!')
 print('Let me guess your age.')
 print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-# reading all remainders
-remainder3 = int(input())
-remainder5 = int(input())
-remainder7 = int(input())
+rem3 = int(input())
+rem5 = int(input())
+rem7 = int(input())
 
-age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-print("Your age is "+str(age)+"; that's a good time to start programming!")
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+
+# read a number and count to it here
+
+print('Completed, have a nice day!')
