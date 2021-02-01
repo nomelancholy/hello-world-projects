39c5d64950b421eb5e7fb31ab57f4fc8ffd1f478
nomelancholy <starmekey@naver.com>
2021-02-01 22:15:24 +0900

    completed project 4

--- python-jetbrains-academy-for-beginners/Simple Chatty Bot/task/bot/bot.py
+++ python-jetbrains-academy-for-beginners/Simple Chatty Bot/task/bot/bot.py
@@ -18,5 +18,10 @@ print("Your age is " + str(age) + "; that's a good time to start programming!")
 print('Now I will prove to you that I can count to any number you want.')
 
 # read a number and count to it here
+n = int(input())
+i = 0
+while i <= n:
+    print(str(i)+"!")
+    i += 1
 
 print('Completed, have a nice day!')
