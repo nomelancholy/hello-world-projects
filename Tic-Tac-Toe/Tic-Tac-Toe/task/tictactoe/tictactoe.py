8c2fbe80a90ab1bdca0f7018b55b0c183071ab09
nomelancholy <starmekey@naver.com>
2021-02-03 19:10:29 +0900

    updated

--- Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,4 +1,8 @@
 # write your code here
-print("X O X")
-print("O X O")
-print("X X O")
+user_input = input()
+print("Enter Cells: "+user_input)
+print("---------")
+print("| "+user_input[0]+" "+user_input[1]+" "+user_input[2]+" |")
+print("| "+user_input[3]+" "+user_input[4]+" "+user_input[5]+" |")
+print("| "+user_input[6]+" "+user_input[7]+" "+user_input[8]+" |")
+print("---------")
