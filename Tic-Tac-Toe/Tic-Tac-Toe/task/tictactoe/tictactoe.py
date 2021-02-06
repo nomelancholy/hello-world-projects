1654f4d07123e1112b8e47e02b4ffd0527f6920b
nomelancholy <starmekey@naver.com>
2021-02-06 22:28:09 +0900

    add python code challenges project and solved problem

--- Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,4 +1,9 @@
 # write your code here
+
+def possible_check(cell_input):
+
+    return 0
+
 user_input = input()
 print("Enter Cells: "+user_input)
 print("---------")
@@ -6,3 +11,4 @@ print("| "+user_input[0]+" "+user_input[1]+" "+user_input[2]+" |")
 print("| "+user_input[3]+" "+user_input[4]+" "+user_input[5]+" |")
 print("| "+user_input[6]+" "+user_input[7]+" "+user_input[8]+" |")
 print("---------")
+
