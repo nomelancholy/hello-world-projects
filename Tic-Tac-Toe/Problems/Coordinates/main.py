22ebb6b1385607b411e729bee33ed198d9e7931a
TakeKnowledge <starmekey@naver.com>
2021-02-11 20:02:04 +0900

    solved today's problem

---
+++ Tic-Tac-Toe/Problems/Coordinates/main.py
@@ -0,0 +1,16 @@
+x = float(input())
+y = float(input())
+
+if x == 0 and y == 0:
+    print("It's the origin!")
+elif x == 0 or y == 0:
+    print("One of the coordinates is equal to zero!")
+else:
+    if x > 0 and y > 0:
+        print("I")
+    elif x < 0 < y:
+        print("II")
+    elif x < 0 and y < 0:
+        print("III")
+    elif x > 0 > y:
+        print("IV")
