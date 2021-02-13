19bda11d320e99581a5e994bdf7452b471f4eeb8
TakeKnowledge <starmekey@naver.com>
2021-02-13 10:31:57 +0900

    completed stage 3

--- Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -14,29 +14,77 @@ def is_sequential(cells):
     else:
         return True
 
+
 def who_won(cells):
 
     x_won = False
     y_won = False
+    blank_cell = 0
 
     for i in range(len(cells)):
-        if i == 0 and cells[i] != '_':
+        if cells[i] == '_':
+            blank_cell += 1
+            continue
+        elif i == 0:
             if cells[0] == cells[1] and cells[1] == cells[2]:
-
-
-
-            cells[i]
+                if cells[i] == 'X':
+                    x_won = True
+                else:
+                    y_won = True
+            elif cells[0] == cells[3] and cells[3] == cells[6]:
+                if cells[i] == 'X':
+                    x_won = True
+                else:
+                    y_won = True
+            elif cells[0] == cells[4] and cells[4] == cells[8]:
+                if cells[i] == 'X':
+                    x_won = True
+                else:
+                    y_won = True
         elif i == 1:
-            cells[i]
+            if cells[1] == cells[4] and cells[4] == cells[7]:
+                if cells[i] == 'X':
+                    x_won = True
+                else:
+                    y_won = True
         elif i == 2:
-            cells[i]
+            if cells[2] == cells[5] and cells[5] == cells[8]:
+                if cells[i] == 'X':
+                    x_won = True
+                else:
+                    y_won = True
+            elif cells[2] == cells[4] and cells[4] == cells[6]:
+                if cells[i] == 'X':
+                    x_won = True
+                else:
+                    y_won = True
         elif i == 3:
-            cells[i]
-        elif i == 5:
+            if cells[3] == cells[4] and cells[4] == cells[5]:
+                if cells[i] == 'X':
+                    x_won = True
+                else:
+                    y_won = True
+        elif i == 6:
+            if cells[6] == cells[7] and cells[7] == cells[8]:
+                if cells[i] == 'X':
+                    x_won = True
+                else:
+                    y_won = True
 
-    return
+    if x_won and y_won:
+        return "Impossible"
+    elif x_won:
+        return "X wins"
+    elif y_won:
+        return "O wins"
+    elif not x_won and not y_won and blank_cell == 0:
+        return "Draw"
+    else:
+        return "Game not finished"
 
-user_input = input()
+
+user_input = input("> ")
+
 print("Enter Cells: "+user_input)
 print("---------")
 print("| "+user_input[0]+" "+user_input[1]+" "+user_input[2]+" |")
@@ -44,8 +92,8 @@ print("| "+user_input[3]+" "+user_input[4]+" "+user_input[5]+" |")
 print("| "+user_input[6]+" "+user_input[7]+" "+user_input[8]+" |")
 print("---------")
 
-
-
 if not is_sequential(user_input):
     print("Impossible")
-elif who_won() == 'X' or 'O':
+else:
+    result = who_won(user_input)
+    print(result)
