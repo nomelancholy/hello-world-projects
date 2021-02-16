51f4be938933c42068b9ef50f57a3a1147ad8ba6
nomelancholy <starmekey@naver.com>
2021-02-16 15:14:53 +0900

    2강 학습내용 정리 및 실습코드

---
+++ c++-univ-lecture/CppPrj/CirclArea/CircleArea.cpp
@@ -0,0 +1,18 @@
+﻿// CirclArea.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const double PI{ 3.14159 };
+    double radius;
+
+    cout << "원의 반경을 입력하시오 : ";
+    cin >> radius;
+
+    double area = radius * radius * PI;
+    cout << "원의 면적 = " << area << endl;
+    return 0;
+}
