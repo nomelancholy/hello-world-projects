943e66760e62679a88ffcd6b3a15a8113491c39d
nomelancholy <starmekey@naver.com>
2021-02-16 23:10:02 +0900

    4강 함수 설명 정리 및 예제 추가

---
+++ c++-univ-lecture/CppPrj/SwapInt/SwapInt.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+void SwapValues(int& x, int& y);
+
+int main()
+{
+    int a, b;
+    cout << "두 수를 입력하시오" << endl;
+    cin >> a >> b;
+    if (a < b) {
+        SwapValues(a, b);
+    }
+
+    cout << "큰 수 = " << a << " 작은 수 = " << b << endl;
+
+    return 0;
+}
+
+void SwapValues(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
