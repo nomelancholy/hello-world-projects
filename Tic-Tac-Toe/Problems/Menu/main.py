aadccf37af62af4cf5275a005846fe54828bb676
nomelancholy <starmekey@naver.com>
2021-02-25 10:29:54 +0900

    solved today's problem

---
+++ Tic-Tac-Toe/Problems/Menu/main.py
@@ -0,0 +1,10 @@
+large = input()
+
+if large == "pizza":
+    print("Margherita, Four Seasons, Neapolitan, Vegetarian, Spicy")
+elif large == "salad":
+    print("Caesar salad, Green salad, Tuna salad, Fruit salad")
+elif large == "soup":
+    print("Chicken soup, Ramen, Tomato soup, Mushroom cream soup")
+else:
+    print("Sorry, we don't have it in the menu")
