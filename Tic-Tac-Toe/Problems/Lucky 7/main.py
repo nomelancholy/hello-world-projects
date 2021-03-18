cc68c6588b1ab144879918c9410b47f0c1941b50
nomelancholy <starmekey@naver.com>
2021-03-18 11:00:41 +0900

    modify answer

--- Tic-Tac-Toe/Problems/Lucky 7/main.py
+++ Tic-Tac-Toe/Problems/Lucky 7/main.py
@@ -5,7 +5,4 @@ answers = []
 for _ in range(n):
     number = int(input())
     if number % 7 == 0:
-        answers.append(number * number)
-
-for answer in answers:
-    print(answer)
+        print(number ** 2)
