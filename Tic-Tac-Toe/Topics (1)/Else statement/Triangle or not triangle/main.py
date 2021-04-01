1a6a3de81c35b28ba40dd39dae299a82b06c7700
TakeKnowledge <starmekey@naver.com>
2021-04-01 09:20:09 +0900

    solved today's problem

---
+++ Tic-Tac-Toe/Topics (1)/Else statement/Triangle or not triangle/main.py
@@ -0,0 +1,8 @@
+first = int(input())
+second = int(input())
+third = int(input())
+
+if first + second + third == 180:
+    print("The triangle is valid!")
+else:
+    print("The triangle is not valid!")
