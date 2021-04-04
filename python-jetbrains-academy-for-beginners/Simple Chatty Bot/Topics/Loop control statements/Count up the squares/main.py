546005bad835c893eb533df949e0426147594042
TakeKnowledge <starmekey@naver.com>
2021-04-04 22:39:29 +0900

    solved today's problem

--- python-jetbrains-academy-for-beginners/Simple Chatty Bot/Topics/Loop control statements/Count up the squares/main.py
+++ python-jetbrains-academy-for-beginners/Simple Chatty Bot/Topics/Loop control statements/Count up the squares/main.py
@@ -1,1 +1,14 @@
 # put your python code here
+numbers_sum = 0
+
+number = int(input())
+numbers_sum += number
+numbers = [number * number]
+
+while numbers_sum != 0:
+    n = int(input())
+    numbers_sum += n
+    numbers.append(n * n)
+
+answer = sum(numbers)
+print(answer)
