def04c218a711ae94c7d4b9db0c3e48142481e41
TakeKnowledge <starmekey@naver.com>
2021-04-18 16:03:27 +0900

    else comprehension

--- Tic-Tac-Toe/Topics/List comprehension/Else comprehension/main.py
+++ Tic-Tac-Toe/Topics/List comprehension/Else comprehension/main.py
@@ -1,5 +1,12 @@
 # the following line reads the list from the input, do not modify it, please
 old_list = [int(num) for num in input().split()]
 
-binary_list = # your code here
+binary_list = []
+
+for num in old_list:
+    if num > 0:
+        binary_list.append(1)
+    else:
+        binary_list.append(0)
+
 print(binary_list)
