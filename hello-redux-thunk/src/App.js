c99c7c21cb241e2fbb3e8f5907d5559c0fba30e7
nomelancholy <starmekey@naver.com>
2021-06-23 12:46:56 +0900

    add postList Component with asyncUtils

--- hello-redux-thunk/src/App.js
+++ hello-redux-thunk/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import CounterContainer from "./containers/CounterContainer";
+// import CounterContainer from "./containers/CounterContainer";
+import PostListContainer from "./containers/PostListContainer";
 
 function App() {
-  return <CounterContainer />;
+  return <PostListContainer />;
 }
 
 export default App;
--- hello-redux-thunk/src/modules/posts.js
+++ hello-redux-thunk/src/modules/posts.js
@@ -1,4 +1,9 @@
 import * as postsAPI from "../api/posts";
+import {
+  createPromiseThunk,
+  reducerUtils,
+  handleAsyncActions,
+} from "../lib/asyncUtils";
 
 /* 액션 타입 */
 
@@ -15,96 +20,119 @@ const GET_POST_ERROR = "GET_POST_ERROR";
 // thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
 // 그냥 thunk 함수에서 바로 액션 객체를 만들어주어도 괜찮습니다.
 
-export const getPosts = () => async (dispatch) => {
-  dispatch({ type: GET_POSTS });
-  try {
-    const post = await postsAPI.getPosts();
-    dispatch({ type: GET_POSTS_SUCCESS, post });
-  } catch (error) {
-    dispatch({ type: GET_POST_ERROR, error });
-  }
-};
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+// export const getPosts = () => async (dispatch) => {
+//   dispatch({ type: GET_POSTS });
+//   try {
+//     const post = await postsAPI.getPosts();
+//     dispatch({ type: GET_POSTS_SUCCESS, post });
+//   } catch (error) {
+//     dispatch({ type: GET_POST_ERROR, error });
+//   }
+// };
 
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST }); // 요청이 시작됨
-  try {
-    const post = await postsAPI.getPostById(id); // API 호출
-    dispatch({ type: GET_POST_SUCCESS, post }); // 성공
-  } catch (e) {
-    dispatch({ type: GET_POST_ERROR, error: e }); // 실패
-  }
-};
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
+// export const getPost = (id) => async (dispatch) => {
+//   dispatch({ type: GET_POST }); // 요청이 시작됨
+//   try {
+//     const post = await postsAPI.getPostById(id); // API 호출
+//     dispatch({ type: GET_POST_SUCCESS, post }); // 성공
+//   } catch (e) {
+//     dispatch({ type: GET_POST_ERROR, error: e }); // 실패
+//   }
+// };
 
 const initialState = {
-  posts: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  post: {
-    loading: false,
-    data: null,
-    error: null,
-  },
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
 };
+
+// const initialState = {
+//   posts: {
+//     loading: false,
+//     data: null,
+//     error: null,
+//   },
+//   post: {
+//     loading: false,
+//     data: null,
+//     error: null,
+//   },
+// };
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: null,
-        },
-      };
     case GET_POSTS_SUCCESS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: action.posts,
-          error: null,
-        },
-      };
     case GET_POSTS_ERROR:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: action.error,
-        },
-      };
+      return handleAsyncActions(GET_POSTS, "posts")(state, action);
     case GET_POST:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: null,
-        },
-      };
     case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: action.post,
-          error: null,
-        },
-      };
     case GET_POST_ERROR:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: action.error,
-        },
-      };
+      return handleAsyncActions(GET_POST, "post")(state, action);
     default:
       return state;
   }
 }
+
+// export default function posts(state = initialState, action) {
+//   switch (action.type) {
+//     case GET_POSTS:
+//       return {
+//         ...state,
+//         posts: {
+//           loading: true,
+//           data: null,
+//           error: null,
+//         },
+//       };
+//     case GET_POSTS_SUCCESS:
+//       return {
+//         ...state,
+//         posts: {
+//           loading: true,
+//           data: action.posts,
+//           error: null,
+//         },
+//       };
+//     case GET_POSTS_ERROR:
+//       return {
+//         ...state,
+//         posts: {
+//           loading: true,
+//           data: null,
+//           error: action.error,
+//         },
+//       };
+//     case GET_POST:
+//       return {
+//         ...state,
+//         post: {
+//           loading: true,
+//           data: null,
+//           error: null,
+//         },
+//       };
+//     case GET_POST_SUCCESS:
+//       return {
+//         ...state,
+//         post: {
+//           loading: true,
+//           data: action.post,
+//           error: null,
+//         },
+//       };
+//     case GET_POST_ERROR:
+//       return {
+//         ...state,
+//         post: {
+//           loading: true,
+//           data: null,
+//           error: action.error,
+//         },
+//       };
+//     default:
+//       return state;
+//   }
+// }
