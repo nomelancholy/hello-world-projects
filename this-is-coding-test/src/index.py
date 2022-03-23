fa9ad9432037e33c2fd6ae63d6927aade9f088ac
TakeKnowledge <starmekey@naver.com>
2022-03-23 23:03:09 +0900

    dummy

--- this-is-coding-test/src/index.py
+++ this-is-coding-test/src/index.py
@@ -1,4 +1,4 @@
-from simulation import game_development, string_rearrangement
+from simulation import game_development, string_rearrangement, dummy
 
-result = string_rearrangement('AJKDLSI412K4JSJ9D')
+result = dummy(10, 4, [[1, 2], [1, 3], [1, 4], [1, 5]], 4, [[8, 'D'], [10, 'D'], [11, 'D'], [13, 'L']])
 print(result)
--- this-is-coding-test/src/simulation.py
+++ this-is-coding-test/src/simulation.py
@@ -1,4 +1,7 @@
 # 118p 게임 개발
+from ast import Continue
+
+
 def game_development(size, now, game_map):
 
     n, m = size
@@ -73,3 +76,76 @@ def string_rearrangement(s):
     answer = string + str(sum(number))
     
     return answer
+
+def dummy(n, k, apples, turn_count, turn_info):
+    
+    board = [[0] * n for _ in range(n)]
+    
+    turn_info.sort(key=lambda x: x[0], reverse=True)
+    
+    # 뱀 세팅
+    board[0][0] = 1
+    head = [0, 0]
+    
+    # 사과 세팅
+    for apple in apples:
+        board[apple[0] - 1][apple[1] - 1] = 9
+    
+    # 방향 정의
+    directions = ['R', 'D', 'L', 'U']
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    # 처음엔 오른 쪽 부터
+    direction = 'R'
+    
+    # 시간
+    time = 0
+    
+    # 뱀 기록
+    record = [(0, 0)]
+    
+    while True:
+        time += 1
+        i = directions.index(direction)
+        
+        nx = dx[i] + head[0]
+        ny = dy[i] + head[1]
+        
+        print(nx, ny, n)
+        
+        # 으앙 주금
+        if nx >= n or ny >= n or nx < 0 or ny < 0 or board[nx][ny] == 1:
+            break
+        # 그냥 이동
+        elif board[nx][ny] == 0:
+            board[nx][ny] = 1
+            record.append((nx, ny))
+            head = [nx, ny]
+            # 꼬리 삭제
+            ox, oy = record.pop(0)
+            board[ox][oy] = 0
+            
+
+        # 사과 먹음
+        elif board[nx][ny] == 9:
+            board[nx][ny] = 1
+            record.append((nx, ny))
+            head = [nx, ny]
+        
+        # 남은 방향 전환 정보 없으면 컨티뉴
+        if not turn_info:
+            continue
+        # 있으면 확인 후 적용
+        if time == turn_info[-1][0]:
+            if turn_info[-1][1] == 'L':
+                i -= 1
+                if i < 0:
+                    i = 3
+            elif turn_info[-1][1] == 'D':
+                i += 1
+                if i > 3:
+                    i = 0
+            direction = directions[i]
+            turn_info.pop()
+        
+    return time
