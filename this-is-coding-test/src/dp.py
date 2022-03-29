78ecb9c3c076f35dd4336accfb65a2b489f497a3
TakeKnowledge <starmekey@naver.com>
2022-03-29 15:13:57 +0900

    [U] 정수 삼각형 클리어

--- this-is-coding-test/src/dp.py
+++ this-is-coding-test/src/dp.py
@@ -127,11 +127,30 @@ def gold_mine():
                         # 그 칸에 이미 기록되어 있던 값과 이동해온 값 사이의 합 중 큰 값을 기록한다
                         max_value[i + 1][ny] = max(max_value[i + 1][ny], max_value[i][j] + gold_mine[i  + 1][ny])
                         
-        print(max_value)
         print(max(max_value[len(max_value) - 1]))
         
-        # for i in range(m):
-            
-            
+def int_triangle():
     
-    print(1)
+    n = int(input())
+
+    triangle = []
+
+    memo = [[0] * i for i in range(1, n + 1)]
+
+    for _ in range(n):
+        triangle.append(list(map(int, input().split())))
+        
+    # 첫 값 저장
+    memo[0][0] = triangle[0][0]
+
+    # 두번째 줄부터
+    for i in range(len(triangle) - 1):
+        # 원소들을 각각 도는데
+        for j in range(len(triangle[i])):
+            # 왼쪽 대각선
+            memo[i + 1][j] = max(memo[i + 1][j], memo[i][j] + triangle[i + 1][j])
+            # 오른쪽 대각선
+            memo[i + 1][j + 1] = max(memo[i + 1][j + 1], memo[i][j] + triangle[i + 1][j + 1])
+            
+    print(max(memo[len(memo) - 1]))            
+        
